b8ff9068feba7651836d94df13f399b0340f72af
VinsTheOne <ivinski@foi.hr>
2020-03-08 20:32:03 +0100

    Main page responsiveness created from scratch, index page responsiveness upgraded, website adjusts too all sizes well.

--- main/main.php
+++ main/main.php
@@ -19,35 +19,38 @@
       </div>
     </header>
 
-    <section class="formContainer">
-      <form action="./main.php" method="post">
-        <textarea type="text" name="title" id="titleInput" placeholder="Title"></textarea><br>
-        <textarea type="text" name="text" id="textInput" placeholder="Post"></textarea><br>
-        <button type="submit" name="postPost">Post</button>
-      </form>
-    </section>
+    <div id="mainContainer">
+      <section class="formContainer">
+        <form action="./main.php" method="post">
+          <textarea type="text" name="title" id="titleInput" placeholder="Title"></textarea><br>
+          <textarea type="text" name="text" id="textInput" placeholder="Post"></textarea><br>
+          <button type="submit" name="postPost">Post</button>
+        </form>
+      </section>
 
-    <?php
-      include "../databaseHandler.php";
-      $conn = connectDB("localhost", "root", "");
-      $userID = getUserID($conn, $_SESSION["username"]);
-      $postCount = getPostsCount($conn, $userID);
-      $posts = getPosts($conn, $userID);
-      for($i = $postCount - 1; $i >= 0; $i--){
-        // DISPLAY ALL POSTS// POST title
-        echo "<section class='postsContainer'>
-          <div class='titleContainer'><h1 class='postTitle'>".$posts[$i][1]."</h1></div>
-          <div class='textContainer'><p class='postText'>".$posts[$i][2]."</p></div>
-          <div class='timestampContainer'><p class='time'>".$posts[$i][3]."</p>
-            <form class='' action='./main.php' method='post'>
-              <input type='hidden' value='".$posts[$i][0]."' name='deleteInput' id='deleteInput'>
-              <button type='submit' name='deleteButton' id='deleteButton'>Delete</button>
-            </form>
-          </div>
-        </section>";
-      }
-      closeConn($conn);
-     ?>
+      <?php
+        include "../databaseHandler.php";
+        $conn = connectDB("localhost", "root", "");
+        $userID = getUserID($conn, $_SESSION["username"]);
+        $postCount = getPostsCount($conn, $userID);
+        $posts = getPosts($conn, $userID);
+        for($i = $postCount - 1; $i >= 0; $i--){
+          // DISPLAY ALL POSTS// POST title
+          echo "<section class='postsContainer'>
+            <div class='titleContainer'><h1 class='postTitle'>".$posts[$i][1]."</h1></div>
+            <div class='textContainer'><p class='postText'>".$posts[$i][2]."</p></div>
+            <div class='timestampContainer'><p class='time'>".$posts[$i][3]."</p>
+              <form class='' action='./main.php' method='post'>
+                <input type='hidden' value='".$posts[$i][0]."' name='deleteInput' id='deleteInput'>
+                <button type='submit' name='deleteButton' id='deleteButton'>Delete</button>
+              </form>
+            </div>
+          </section>";
+        }
+        closeConn($conn);
+       ?>
+
+    </div>
 
     <footer>
       <div class="container">
